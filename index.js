7dc91877d182cb2709c942969f334608b0a71ba1
ugi ispoyo widodo <ugi.ispoyowidodo@gmail.com>
2018-01-12 14:55:43 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { AppRegistry } from 'react-native';
-// import App from './App';
+import App from './App';
+
+// Untuk IOS & ANDROID
 import Latihanprops from './components/Latihanprops';
 import MultipleGreeting from './components/MultipleGreeting';
 import LatihanState from './components/LatihanState';
@@ -9,6 +11,11 @@ import LatihanButton from './components/LatihanButton';
 import LatihanTouchable from './components/LatihanTouchable';
 import LatihanVerticalScrollView from './components/LatihanVerticalScrollView';
 import LatihanHorizontalScrollView from './components/LatihanHorizontalScrollView';
+
+// Untuk ANDROID
 import LatihanViewPager from './components/LatihanViewPager';
 
 AppRegistry.registerComponent('LatihanPertama', () => LatihanViewPager);
+
+
+// BY UGI ISPOYO WIDODO
